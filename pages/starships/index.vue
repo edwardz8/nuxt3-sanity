a40d6109915b459e1d22834ad4f7426655685218
zne <edwardszm@ornl.gov>
2022-10-13 16:44:55 -0400

    add home btn

--- pages/starships/index.vue
+++ pages/starships/index.vue
@@ -57,6 +57,14 @@ const showNextPage = computed(() => {
                 <span v-else>Previous</span>
             </a>
 
+            <NuxtLink to="/">
+                <div class="rounded-md shadow">
+                    <a href="#"
+                        class="flex w-full items-center justify-center rounded-md border border-transparent bg-orange-500 px-8 py-3 text-base font-medium text-white hover:bg-orange-800 md:py-2 md:px-6 md:text-lg">
+                        Home</a>
+                </div>
+            </NuxtLink>
+
             <a href="#" :disabled="showNextPage === 0" :class="[
               showNextPage === 0
                 ? 'border-gray-300 text-gray-500'
